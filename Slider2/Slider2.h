e8a2b7e8d22840674e12c5b3d0d7a76c08ce8606
karanokk <karanokk@icloud.com>
2020-05-30 16:36:07 +0800

    Initial Commit

---
+++ Slider2/Slider2.h
@@ -0,0 +1,19 @@
+//
+//  Slider2.h
+//  Slider2
+//
+//  Created by byunfi on 2020/5/30.
+//  Copyright © 2020 byunfi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Slider2.
+FOUNDATION_EXPORT double Slider2VersionNumber;
+
+//! Project version string for Slider2.
+FOUNDATION_EXPORT const unsigned char Slider2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Slider2/PublicHeader.h>
+
+
